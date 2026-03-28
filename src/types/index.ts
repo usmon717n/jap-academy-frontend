44e5705691fb979b9b2c52f2ed7fc80e0ffdf141
Usmon Umaraliyev <usmon717n@gmail.com>
2026-03-28 12:48:28 +0500

    teachers

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,7 @@ export interface User {
   firstName: string;
   lastName: string;
   phone: string;
-  role: 'STUDENT' | 'ADMIN';
+  role: 'STUDENT' | 'ADMIN' | 'TEACHER';
 }
 
 export interface AuthResponse {
